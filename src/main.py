d6407144724ae5618e82c9622ae322f5b3c5541a
landinij0040 <ijlandin@miners.utep.edu>
2021-09-05 23:12:15 -0600

    main.py: Added Classes ExportData, AnnotateDate, SearchData. Added The classes to the GUI content container. ui: added ExportData, AnnotateData, and Search UI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import sys, os
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget,QMainWindow, QHBoxLayout, QFrame,QPushButton
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget,QMainWindow, QHBoxLayout, QFrame, QPushButton, QStackedWidget
+
+class ExportData(QWidget):
+    def __init__(self):
+        super().__init__()
+        loadUi(os.getcwd() + "/views/ExportData.ui", self)
+
+class AnnotateData(QWidget):
+    def __init__(self):
+        super().__init__()
+        loadUi(os.getcwd() + "/views/AnnotateData.ui", self)
+
+class SearchWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        loadUi(os.getcwd() + "/views/Search.ui", self)
+
 
 class OuterFrame(QMainWindow):#would not work as a QDialog for some reason had to be a QMainWindow to load
     def __init__(self):
@@ -16,27 +32,51 @@ class OuterFrame(QMainWindow):#would not work as a QDialog for some reason had t
         self.Settings_Btn.clicked.connect()
         self.Visualize_Btn.clicked.connect()
         self.Transactions_Btn.clicked.connect()
-        self.Export_Btn.clicked.connect()
         self.Delete_Btn.clicked.connect()
         self.Search_Left_Frame_Btn.clicked.connect()
-        self.Annotate_Btn.clicked.connect()
         self.Keystroke_Btn.clicked.connect()
         self.Video_Btn.clicked.connect()
         self.ScreenShot_Btn.clicked.connect()
         self.Mouse_Action_Btn.clicked.connect()
         self.NetworkData_Btn.clicked.connect()
         '''
+        self.Export_Btn.clicked.connect(self.exportDataClickHandler)
+        self.Annotate_Btn.clicked.connect(self.annotateDataClickHandler)
+        self.Search_Left_Frame_Btn.clicked.connect(self.searchClickHandler)
 
-        # Used to add other ui elements to the other frames
-        # self.Content_Container
-        self.Content_Container.setStyleSheet('background: red ;')
-        btn = QPushButton("Oh please god press me ")
+        # The Code Below is to setup the various pages for changing the content pages
+
+        # the layout to hold the QStackedWidget
         layout = QHBoxLayout()
-        layout.addWidget(btn)
 
+        # Stacked Widget to Contain the content to be changed based on the coresponding button presses
+        self.contentStackedWidget = QStackedWidget()
+
+        # Adding The Export Widget
+        self.ExportDataWidget = ExportData()
+        self.contentStackedWidget.addWidget(self.ExportDataWidget) # index 0
+
+        # Adding The Annotate Widget
+        self.AnnotateDataWidget = AnnotateData()
+        self.contentStackedWidget.addWidget(self.AnnotateDataWidget) # index 1
+
+        # Adding The Search Widget
+        self.SearchWidget = SearchWidget()
+        self.contentStackedWidget.addWidget(self.SearchWidget) # index 2
+
+        layout.addWidget(self.contentStackedWidget)
         self.Content_Container.setLayout(layout)
 
+    def exportDataClickHandler(self):  # index 0
+        self.contentStackedWidget.setCurrentIndex(0)
+        print("data")
 
+    def annotateDataClickHandler(self):  # index 1
+        self.contentStackedWidget.setCurrentIndex(1)
+
+    def searchClickHandler(self):  # index 2
+        self.contentStackedWidget.setCurrentIndex(2)
+        print("Search")
 
 #main
 app = QApplication(sys.argv)
