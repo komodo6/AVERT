6a12d55f0948195d7830be021f834b53dd6c8c8c
Michael Fernandez <mjfernandez3@miners.utep.edu>
2021-09-13 16:37:57 -0600

    packet capture added

--- src/views/PacketCapture/PacketCaptureTable.py
+++ src/views/PacketCapture/PacketCaptureTable.py
@@ -1,12 +1,23 @@
-
+import uuid
+from posixpath import pardir
 import sys
 import os
-from PyQt5 import uic
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5 import uic, QtCore
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QVBoxLayout, QListWidget, QTableWidgetItem, QTableView, QCheckBox, QAbstractItemView
 from functools import partial
-
+from datetime import datetime
+import datetime
 current_dir = os.path.dirname(os.path.abspath(__file__))
+
+data = [('packet-121', datetime.datetime(2019, 5, 5, 0, 54)),
+        ('packet-121', datetime.datetime(2019, 5, 5, 0, 54)),
+        ('packet-121', datetime.datetime(2019, 5, 5, 0, 54)),
+        ('packet-121', datetime.datetime(2019, 5, 5, 0, 54)),
+        ('packet-121', datetime.datetime(2019, 5, 5, 0, 54)),
+        ('packet-121', datetime.datetime(2019, 5, 5, 0, 54)),
+        ('packet-121', datetime.datetime(2019, 5, 5, 0, 54))]
 
 
 Form, Base = uic.loadUiType(os.path.join(
@@ -16,7 +27,30 @@ Form, Base = uic.loadUiType(os.path.join(
 class PacketCaptureTableWidget(Base, Form):
     def __init__(self, parent=None):
         super(self.__class__, self).__init__(parent)
-        self.setupUi(self)
+        self.setupUi()
+
+    def setupUi(self):
+        model = QStandardItemModel()
+
+        model.setHorizontalHeaderLabels(
+            ['Name', 'Timestamp'])
+        model.setHeaderData(0, QtCore.Qt.Horizontal,
+                            QtCore.Qt.AlignCenter, QtCore.Qt.TextAlignmentRole)
+
+        for row in range(len(data)):
+            for column in range(len(data[row])):
+                item = QStandardItem(str(data[row][column]))
+                # item.setSelectable(True)
+                # item.setCheckable(True)
+                item.setEditable(False)
+                model.setItem(row, column, item)
+
+        table = QTableView()
+        table.setModel(model)
+        table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(table)
+        self.setLayout(vbox)
 
 
 if __name__ == '__main__':
