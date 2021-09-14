a1bceec4a25adfed7447512a47cc839655d38e3c
Cristobal Medina <cristobalmd01@gmail.com>
2021-09-14 16:36:27 -0600

    Finished all views

--- src/views/Video/Video.py
+++ src/views/Video/Video.py
@@ -1,33 +1,41 @@
-import sys, os
+import sys
+import os
 from PyQt5 import uic
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QVBoxLayout, QLabel
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QVBoxLayout
 from functools import partial
+
+currentView = 0
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-Form, Base = uic.loadUiType(os.path.join(current_dir, "../../ui/Video/VideoGrid.ui"))
+
+Form, Base = uic.loadUiType(os.path.join(
+    current_dir, "../../ui/Video/Video.ui"))
+
 
 class VideoWidget(Base, Form):
-	def __init__(self, parent=None):
-		super(self.__class__, self).__init__(parent)
-		self.setupUi(self)
-         
-        # loading image
-		self.pixmap = QPixmap(os.path.join(current_dir, "../../img/vid.png"))
 
-		positions = [(i, j) for i in range(2) for j in range(4)]
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        self.setupUi(self)
+        self.currentView = 0
+        self.viewButton.clicked.connect(self.switchViews)
 
-		for position in positions:
-			# adding image to label
-			label = QLabel(self)
-			label.setPixmap(self.pixmap)
-			self.videoGrid.addWidget(label, *position)
+    def switchViews(self):
+        if(self.currentView == 0):
+            self.videoViews.setCurrentIndex(1)
+            self.currentView = 1
+            self.viewButton.setText("List View")
+        elif(self.currentView == 1):
+            self.videoViews.setCurrentIndex(0)
+            self.currentView = 0
+            self.viewButton.setText("Grid View")
+
 
 if __name__ == '__main__':
-	import sys
-	app = QApplication(sys.argv)
-	w = VideoWidget()
-	w.show()
-	sys.exit(app.exec_())
+    import sys
+    app = QApplication(sys.argv)
+    w = VideoWidget()
+    w.show()
+    sys.exit(app.exec_())
