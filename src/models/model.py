4078b77f31d9a1e03d823baeed11eaceec8181b5
landinij0040 <ijlandin@miners.utep.edu>
2021-10-01 12:21:14 -0600

    Changed the Superclass to Database name to DatabaseCollection Added functionality to insert a mouse action document into the mongo db database

--- src/models/model.py
+++ src/models/model.py
@@ -1,20 +1,28 @@
 
 import pymongo
 import json
-
-
 '''
 Artifact Classes
 '''
-# Super Class Artifact
-# TODO: FINISH the artifact class
-# TODO all the artifact classes have an anotation
-# class: Artifact:
-#     def __init__
-
-
-class Keystroke:
+# Super Class
+class Artifact:
+    _annotations = ""
+    _time_created = ""
     def __init__(self):
+        pass
+
+# Sub Class
+class Keystroke(Artifact):
+    def __init__(self):
+        super().__init__()
+        self.test = 'test'
+
+    def data_to_dict(self):
+        return {'test' : self.test}
+
+class MouseAction(Artifact):
+    def __init__(self):
+        super().__init__()
         self.test = 'test'
 
     def data_to_dict(self):
@@ -26,31 +34,40 @@ Database Classes
 '''
 
 # Super Class
-class Database:
+class DatabaseCollection:
     def __init__(self):
         self.client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
         # Accessing the AVERT database
         self.database = self.client['AVERT']
-    def data_to_json(self, data): # TODO: delete later
-        return json.dumps(data)
-
 
 # Sub Classes
-class KeystrokeCollection(Database):
+class KeystrokeCollection(DatabaseCollection):
     def __init__(self):
         super().__init__()
         # Accessing the Keystroke collection
-        self.collection = self.database.keystroke
+        self.collection = self.database.keystrokes
 
     def create(self, keystroke):
         self.collection.insert_one(keystroke.data_to_dict())
 
-
     #TODO: implement the below
     # def read
     # def update
-    # def deleteSS
+    # def delete
 
+class MouseActionCollection(DatabaseCollection):
+    def __init__(self):
+        super().__init__()
+        # Accessing the Mouse Action collection
+        self.collection = self.database.mouse_actions
+
+    def create(self, mouse_action):
+        self.collection.insert_one(mouse_action.data_to_dict())
+
+    # TODO: implement the below
+    # def read
+    # def update
+    # def delete
 
 # Testing
 if __name__ == '__main__':
@@ -58,5 +75,6 @@ if __name__ == '__main__':
     keystroke1 = Keystroke()
     keystroke_collection.create(keystroke1)
 
-    # showing the databases
-    print(keystroke_collection.client.list_database_names())
+    mouse_action_collection = MouseActionCollection()
+    mouse_action1 = MouseAction()
+    mouse_action_collection.create(mouse_action1)
