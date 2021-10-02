efadcb2231ae6bb7284768047b6689218328fa88
Michael Fernandez <mjfernandez3@miners.utep.edu>
2021-10-02 12:33:55 -0600

    added models

---
+++ src/models/Artifact.py
@@ -0,0 +1,12 @@
+class Artifact:        
+    def __init__(self, timestamp, ip_address, mac_address, annotations):
+        self.timestamp = timestamp
+        self.ip_address = ip_address
+        self.mac_address = mac_address
+        self.annotations = annotations
+
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+       
