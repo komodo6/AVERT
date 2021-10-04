364a3a593cc262ef6335edb954e041ba95cca211
mrryzen <cristobalmd01@gmail.com>
2021-10-04 16:34:48 -0600

    replace radial with toggle

--- src/views/Settings/Settings.py
+++ src/views/Settings/Settings.py
@@ -1,7 +1,7 @@
 import sys, os
 from PyQt5 import uic
-from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget,QMainWindow,QVBoxLayout
+from qtwidgets import AnimatedToggle
 from functools import partial
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,6 +14,15 @@ class SettingsWidget(Base, Form):
 	def __init__(self, parent=None):
 		super(self.__class__, self).__init__(parent)
 		self.setupUi(self)
+
+		for i in range(0, 6):
+			toggle = AnimatedToggle(
+            	checked_color="#00FF00",
+            	pulse_checked_color="#00FF00",
+				pulse_unchecked_color="#FF0000"
+        	)
+			self.gridLayout_2.addWidget(toggle, i, 1)
+
 
 
 if __name__ == '__main__':
