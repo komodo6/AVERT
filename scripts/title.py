bb95d6d977971502e4d12b03c510dcb4585689aa
Michael Fernandez <mjfernandez3@miners.utep.edu>
2021-10-05 10:33:58 -0600

    added window focus

---
+++ scripts/title.py
@@ -0,0 +1,8 @@
+from gi.repository import Gtk, Wnck
+
+# from
+# gi.require_version("Wnck", "3.0")
+
+# scr = wnck.Screen.get_default()
+# scr.force_update()
+# print(scr.get_active_window().get_name())
