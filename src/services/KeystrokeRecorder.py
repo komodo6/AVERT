95e95c47c3fad432a710acd6b8e8aec807c5735b
Cristobal Medina <cristobalmd01@gmail.com>
2021-10-05 12:29:14 -0600

    connected toggles in settings

--- src/services/KeystrokeRecorder.py
+++ src/services/KeystrokeRecorder.py
@@ -1,6 +1,6 @@
 from services.Recorder import Recorder
 from db.KeystrokeDAO import KeystrokeDAO
-import pynput
+from pynput import keyboard
 from models.Keystoke import Keystroke
 from models.Annotation import Annotation
 
@@ -11,22 +11,22 @@ class KeystrokeRecorder(Recorder):
         self.ip = super().get_ip()
         self.mac = super().get_mac()
         self.ksDAO = KeystrokeDAO()
-        self.listener = pynput.keyboard.Listener(on_release=self.on_release)
+        self.running = False
+        self.listener = keyboard.Listener(on_release=self.on_release)
         self.listener.start()
-        self.listener.join()
 
     def on_release(self, key):
-        try:
-            self.save_keystroke(str(key.char))
-        except Exception as e:
-            raise e
+        if self.running:
+            try:
+                self.save_keystroke(str(key.char))
+            except Exception as e:
+                raise e
 
     def save_keystroke(self, key):
         Keystroke(timestamp=super().get_timestamp(), ip_address=self.ip,
                   mac_address=self.mac, annotation=Annotation(self.ip, None), key=str(key.char))
 
     def stop(self):
-        self.listener.stop()
-
+        self.running = False
     def start(self):
-        self.listener.start()
+        self.running = True
