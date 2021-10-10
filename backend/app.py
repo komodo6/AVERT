a80333f24b64480df704c402cdc4aae2b0ebb8dc
Michael Fernandez <mjfernandez3@miners.utep.edu>
2021-10-10 15:46:31 -0600

    added export and table views for mouse actions and keystrokes

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from app import create_app
+from flask_cors import CORS
 
 
 app = create_app()
+CORS(app)
+
 
 @app.route('/')
 def index():
     return "Hello World"
-
-
-
-
 
 
 if __name__ == '__main__':
