c1e5fb099fee8dc29c0f8af1286847169d72520d
Michael Fernandez <mjfernandez3@miners.utep.edu>
2021-10-10 00:15:15 -0600

    added tables to the rest of the views

---
+++ frontend/src/pages/Screenshots/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="q-pa-sm">
+    <div class="row">
+      <div class="col">
+        <q-table :columns="columns"> </q-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const columns = [
+  {
+    name: "timestamp",
+    label: "Date/Tme",
+    sortable: true,
+    align: "center",
+  },
+  {
+    name: "IP Address",
+    label: "IP Address",
+    align: "center",
+    sortable: true,
+  },
+  {
+    name: "MAC Address",
+    label: "MAC Address",
+    align: "center",
+    sortable: true,
+  },
+  {
+    name: "Key",
+    label: "Key",
+    align: "center",
+    sortable: true,
+  },
+  {
+    name: "Active Window",
+    label: "Active Window",
+    align: "center",
+    sortable: true,
+  },
+];
+export default {
+  setup() {
+    return {
+      columns,
+    };
+  },
+};
+</script>
