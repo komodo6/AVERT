2dec7ebea13b942f260bd3c7f8dcf308fa40c3c1
landinij0040 <ijlandin@miners.utep.edu>
2021-10-11 09:12:56 -0600

    Got ScreenShot.py Prototype to work

--- src/services/ScreenShot.py
+++ src/services/ScreenShot.py
@@ -1,5 +1,20 @@
 from Recorder import Recorder
+import numpy as np
+import cv2
+# pip install opencv-python
+# sudo apt-get install scrot
+# pip install Pillow
+import pyautogui
 
 class ScreenShot(Recorder):
     def __init__(self):
         print("Hello World")
+
+
+
+if __name__ == "__main__":
+    image = pyautogui.screenshot()
+
+    image = cv2.cvtColor(np.array(image),
+                         cv2.COLOR_RGB2BGR)
+    cv2.imwrite("image1.png", image)
