0160a9280dffd3e67501e2ae8642383e6f2b89fd
landinij0040 <ijlandin@miners.utep.edu>
2021-10-11 09:22:36 -0600

    Got ScreenShot.py Prototype 2.0 to work

--- src/services/ScreenShot.py
+++ src/services/ScreenShot.py
@@ -8,13 +8,23 @@ import pyautogui
 
 class ScreenShot(Recorder):
     def __init__(self):
-        print("Hello World")
+        self.ip = super().get_ip()
+        self.mac = super().get_mac()
+        self.timestamp = None
 
+
+    def take_screen_shot(self):
+        image = pyautogui.screenshot()
+        image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
+        cv2.imwrite("image1.png", image)
 
 
 if __name__ == "__main__":
-    image = pyautogui.screenshot()
+    # image = pyautogui.screenshot()
+    #
+    # image = cv2.cvtColor(np.array(image),
+    #                      cv2.COLOR_RGB2BGR)
+    # cv2.imwrite("image1.png", image)
 
-    image = cv2.cvtColor(np.array(image),
-                         cv2.COLOR_RGB2BGR)
-    cv2.imwrite("image1.png", image)
+    screen_shot = ScreenShot()
+    screen_shot.take_screen_shot()
