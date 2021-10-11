dbd50c2f513ce5eeee5009eefebc6bcc42bec0b6
landinij0040 <ijlandin@miners.utep.edu>
2021-10-11 12:08:32 -0600

    Got ScreenShot.py Prototype 3.0 to work

--- src/services/ScreenShot.py
+++ src/services/ScreenShot.py
@@ -1,10 +1,18 @@
 from Recorder import Recorder
+
 import numpy as np
+
 import cv2
 # pip install opencv-python
 # sudo apt-get install scrot
 # pip install Pillow
+
+
 import pyautogui
+import gridfs
+from db.db import db
+import io
+from PIL import Image
 
 class ScreenShot(Recorder):
     def __init__(self):
@@ -15,16 +23,43 @@ class ScreenShot(Recorder):
 
     def take_screen_shot(self):
         image = pyautogui.screenshot()
-        image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
-        cv2.imwrite("image1.png", image)
+        image = image.convert()
+        image = image.convert()
+        image.show()
+        # TODO: make this used as export below
+        # image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
+        # cv2.imwrite("image1.png", image)
+
+
+        # fs = gridfs.GridFS(db, collection='ScreenShots')
+        # fs.put(image)
+
 
 
 if __name__ == "__main__":
-    # image = pyautogui.screenshot()
-    #
-    # image = cv2.cvtColor(np.array(image),
-    #                      cv2.COLOR_RGB2BGR)
+
+    # Getting the screen shot
+    image = pyautogui.screenshot()
+    # image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
     # cv2.imwrite("image1.png", image)
 
-    screen_shot = ScreenShot()
-    screen_shot.take_screen_shot()
+    # change the screenshot to bytes
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, format='PNG')
+    image = {
+        'data': image_bytes.getvalue()
+    }
+    # putting the screen shot into the database
+    image_id = db.ScreenShots.insert_one(image).inserted_id
+
+    # geting the current image from the database
+    image = db.ScreenShots.find_one({"_id":image_id})
+    image = Image.open(io.BytesIO(image['data']))
+
+    # saving that file into the folder
+    image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
+    cv2.imwrite("image1.png", image)
+
+    # screen_shot = ScreenShot()
+    # screen_shot.take_screen_shot()
+
