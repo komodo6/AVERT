bfd3da33b12273c25d60a71b7dac176eb9d1d3ee
SethFS99 <sethfloresgamerg33k@gmail.com>
2021-10-17 20:17:14 -0600

    Made changes to home vue

--- frontend/src/pages/Home/Index.vue
+++ frontend/src/pages/Home/Index.vue
@@ -9,63 +9,44 @@
           row-key="name"
           dark
           color="amber"
+          :rows-per-page-options="[]"
+
+
+          scroll
         >
           <template v-slot:top>
-            <q-input
-              dark
-              dense
-              standout
-              v-model="text"
-              input-class="text-right"
-              class="q-ml-md"
-              label="Search"
-            >
+            <q-input dark dense standout v-model="text" input-class="text-left" class="q-ml-md" label="Search" >
               <template v-slot:append>
                 <q-icon v-if="text === ''" name="search" />
-                <q-icon
-                  v-else
-                  name="clear"
-                  class="cursor-pointer"
-                  @click="text = ''"
-                />
+                <q-icon v-else name="clear" class="cursor-pointer" @click="text = ''" />
               </template>
             </q-input>
             <div class="item-selections row q-mx-sm">
               <div v-if="$q.screen.gt.xs" class="col">
-                <q-toggle
-                  v-model="visibleColumns"
-                  val="calories"
-                  label="Calories"
-                />
-                <q-toggle v-model="visibleColumns" val="fat" label="Fat" />
-                <q-toggle v-model="visibleColumns" val="carbs" label="Carbs" />
-                <q-toggle
-                  v-model="visibleColumns"
-                  val="protein"
-                  label="Protein"
-                />
-                <q-toggle
-                  v-model="visibleColumns"
-                  val="sodium"
-                  label="Sodium"
-                />
-                <q-toggle
-                  v-model="visibleColumns"
-                  val="calcium"
-                  label="Calcium"
-                />
-                <q-toggle v-model="visibleColumns" val="iron" label="Iron" />
+                <q-checkbox v-model="visibleColumns" val="All" label="All" />
+                <q-checkbox v-model="visibleColumns" val="Keystrokes" label="Keystrokes"/>
+                <q-checkbox v-model="visibleColumns" val="Mouse Actions" label="Mouse Actions" /> <!-- q toggles are the toggles at the top-->
+                <q-checkbox v-model="visibleColumns" val="Window Actions" label="Window Actions"/>
+                <q-checkbox v-model="visibleColumns" val="System Calls" label="System Calls"/>
+                <q-checkbox v-model="visibleColumns" val="Processes" label="Processes"/>
+                <q-checkbox v-model="visibleColumns" val="PCAPs" label="PCAPs"/>
+                <q-checkbox v-model="visibleColumns" val="Videos" label="Videos" />
+                <q-checkbox v-model="visibleColumns" val="Screenshots" label="Screenshots" />
               </div>
             </div>
 
             <div class="row">
-              <div class="datetime-from col-12">
-                <input type="datetime-local" name="" id="" />
+              <div class="datetime-from col-13" style = "padding: 15px">
+                From:&nbsp;
+                <input type="datetime-local" step = "1" name="Start" id="Start" />
               </div>
-              <div class="datetime-to col-12">
-                <input type="datetime-local" name="" id="" />
+              <div class="datetime-to col-13" style = "padding: 15px">
+                To:&nbsp;
+                <input type="datetime-local" step = "1" name="End" id="End" />
               </div>
             </div>
+              <q-input outlined v-model="text" dense = "dense" label="IP Address" style = "width: 350px; padding: 15px"/>
+              <q-input outlined v-model="text" dense = "dense" label="MAC Address" style = "width: 350px; padding: 15px"/>
           </template>
         </q-table>
       </div>
@@ -77,166 +58,39 @@
 import { defineComponent, ref, computed } from "vue";
 
 const columns = [
-  {
-    name: "name",
-    required: true,
-    label: "Dessert (100g serving)",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: "calories",
-    align: "center",
-    label: "Calories",
-    field: "calories",
-    sortable: true,
-  },
-  { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
-  { name: "carbs", label: "Carbs (g)", field: "carbs" },
-  { name: "protein", label: "Protein (g)", field: "protein" },
-  { name: "sodium", label: "Sodium (mg)", field: "sodium" },
-  {
-    name: "calcium",
-    label: "Calcium (%)",
-    field: "calcium",
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  },
-  {
-    name: "iron",
-    label: "Iron (%)",
-    field: "iron",
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  },
+  { name: "Checkboxes", label: "_", field: "Checkboxes", align: "left" },
+  { name: "Date", required: true, label: "Date", align: "left", field: (row) => row.name, format: (val) => `${val}`, sortable: true},
+  { name: "Time", align: "center", label: "Time", field: "Time", sortable: true},
+  { name: "DataType", label: "Data Type", field: "DataType"},
+  { name: "MACAddress", label: "MAC Address", field: "MACAddress" },
+  { name: "IPAddress", label: "IP Address", field: "IPAddress" },
 ];
 
 const rows = [
-  {
-    name: "Frozen Yogurt",
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: "14%",
-    iron: "1%",
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: "8%",
-    iron: "1%",
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: "6%",
-    iron: "7%",
-  },
-  {
-    name: "Cupcake",
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: "3%",
-    iron: "8%",
-  },
-  {
-    name: "Gingerbread",
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: "7%",
-    iron: "16%",
-  },
-  {
-    name: "Jelly bean",
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: "0%",
-    iron: "0%",
-  },
-  {
-    name: "Lollipop",
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: "0%",
-    iron: "2%",
-  },
-  {
-    name: "Honeycomb",
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: "0%",
-    iron: "45%",
-  },
-  {
-    name: "Donut",
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: "2%",
-    iron: "22%",
-  },
-  {
-    name: "KitKat",
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: "12%",
-    iron: "6%",
-  },
+  {name: "4/20/2021", Time: "13:50:30", DataType: "Mouse Action", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/20/2021", Time: "13:50:29", DataType: "Mouse Action", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:23", DataType: "Keystroke", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:23", DataType: "Keystroke", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:23", DataType: "Keystroke", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:23", DataType: "Process", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:23", DataType: "System Call", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:22", DataType: "Window Action", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:22", DataType: "Mouse Action", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "4/19/2021", Time: "12:55:22", DataType: "Mouse Action", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "3/19/2021", Time: "13:45:22", DataType: "Process", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "3/19/2021", Time: "13:45:33", DataType: "Process", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "3/19/2021", Time: "13:45:32", DataType: "Process", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "3/19/2021", Time: "12:45:32", DataType: "PCAP", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "3/19/2021", Time: "12:45:32", DataType: "PCAP", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
+  {name: "3/19/2021", Time: "12:45:32", DataType: "Keystroke", MACAddress: "30-65-EC-6F-C4-58", IPAddress: "129.102.99.234",},
 ];
 
 export default defineComponent({
   setup() {
-    let visibleColumns = ref([
-      "calories",
-      "desc",
-      "fat",
-      "carbs",
-      "protein",
-      "sodium",
-      "calcium",
-      "iron",
-    ]);
-
+    let visibleColumns = ref(["Time","DataType","MACAddress","IPAddress","Date",]);
     const plusOne = computed(() => visibleColumns);
     console.log(plusOne);
-    return {
-      visibleColumns,
-      rows,
-      columns,
-      text: ref(""),
-    };
+    return {visibleColumns,rows,columns,text: ref(""),};
   },
 });
 </script>
