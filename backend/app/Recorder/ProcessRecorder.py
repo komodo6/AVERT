8ecf84b34ab909d9643f7541d208023cbe204516
Cristobal Medina <cristobalmd01@gmail.com>
2021-10-19 16:02:23 -0600

    fixed process

--- backend/app/Recorder/ProcessRecorder.py
+++ backend/app/Recorder/ProcessRecorder.py
@@ -25,14 +25,15 @@ class ProcessRecorder(Recorder):
         # Checking  if thread should stop
         while not self.exit_event.is_set():
         # Finding all processes
-            try:
-                for proc in psutil.process_iter():
+            for proc in psutil.process_iter():
+                try:
                     if proc not in self.pro_dict:
                         # Getting all relevant process information
                         new_process = Process(
                             super().get_timestamp(),
                             super().get_ip(),
                             super().get_mac(),
+                            None,
                             None,
                             proc.name(),
                             proc.username(),
@@ -44,17 +45,17 @@ class ProcessRecorder(Recorder):
                             proc.status(),
                             proc.status(),
                             proc.memory_percent(),
-                            proc.terminal(),
+                            proc.threads(),
                             proc.cpu_percent(),
-                            os.getuid() == 0,
+                            None,
                             proc.nice()
                         )
                         self.process_collection.create(new_process)
                         self.pro_dict[new_process] = proc.pid
                     else:
                         pass
-            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                pass
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                    pass
         self.pro_dict = {}
         sys.exit()
 
