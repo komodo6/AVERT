08d7c958ab2bc64b9fa25824401d0f711a9db9f3
landinij0040 <ijandin@miners.utep.edu>
2021-10-19 08:38:30 -0600

    Window history recorder prototype

---
+++ backend/app/Recorder/WindowHistoryRecorder.py
@@ -0,0 +1,12 @@
+import subprocess
+cmd = ['xrandr']
+cmd2 = ['grep', '*']
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+p2 = subprocess.Popen(cmd2, stdin=p.stdout, stdout=subprocess.PIPE)
+p.stdout.close()
+resolution_string, junk = p2.communicate()
+resolution = resolution_string.split()[0]
+width, height = resolution.split(b'x')
+
+print(width.decode())
+print(height.decode())
