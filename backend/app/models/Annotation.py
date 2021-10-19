1733e2bb41595ff1f33c1bf4c6c53b0c929f3b24
landinij0040 <ijandin@miners.utep.edu>
2021-10-18 22:39:14 -0600

    Updated Annotation

--- backend/app/models/Annotation.py
+++ backend/app/models/Annotation.py
@@ -1,10 +1,21 @@
 class Annotation:
-    def __init__(self, ip_address, note) -> None:
+    def __init__(self, ip_address) -> None:
         self.ip_address = ip_address
-        self.note = note
+        self.annotations = []
+
+    def add_annotations(self, ip, text):
+        self.annotations = self.annotations + [{'ip':ip,'text':text}]
 
     def __getitem__(self, item):
         return getattr(self, item)
 
     def toJSON(self):
         return self.__dict__
+
+    
+if __name__ == '__main__':
+    ann = Annotation('1.1.1.1')
+    ann.add_annotations(ann.ip_address, 'this is text1')
+    ann.add_annotations(ann.ip_address, 'this is text2')
+    ann.add_annotations(ann.ip_address, 'this is text3SS')
+    print(ann.toJSON())
