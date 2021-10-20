cd4d7a384573205bdd34a69b14504f6618926555
landinij0040 <ijandin@miners.utep.edu>
2021-10-19 22:57:55 -0600

    Making WindowHistory Objects Work

--- backend/app/Recorder/WindowHistoryRecorder.py
+++ backend/app/Recorder/WindowHistoryRecorder.py
@@ -8,13 +8,37 @@ from gi.repository import GObject, Wnck
 
 import time #TODO: Delete Later
 from .Recorder import Recorder
+import subprocess
+from ..models.WindowHistory import WindowHistory
+from ..Process.ProcessDAO import ProcessDAO
 
 class WindowHistoryRecorder(Recorder):
+    
+    def get_screen_res(self):
+        cmd = ['xrandr']
+        cmd2 = ['grep', '*']
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(cmd2, stdin=p.stdout, stdout=subprocess.PIPE)
+        p.stdout.close()
+        resolution_string, junk = p2.communicate()
+        resolution = resolution_string.split()[0]
+        width, height = resolution.split(b'x')    
+        return [{'width':width.decode()}, {'height':height.decode()}] 
+
     def creation(self,this_screen: Wnck.Screen, opened_window: Wnck.Window):
         if self.rec:
             try:
-                print('hello')
-                self.wh_dict[opened_window] = {opened_window.get_name()}
+                print('hello') # TODO: delete
+                new_window = WindowHistory(
+                    self.get_timestamp(),
+                    self.get_ip(),
+                    self.get_mac(),
+                    None,
+                    None,
+                    self.res
+                )
+                self.wh_dict[new_window] = {new_window}
+                print(self.res)
                 # app: self.Wnck.Application = opened_window.get_application()
                 # app_name = app.get_name()
                 # print('app name -> ' + app_name)
@@ -51,6 +75,7 @@ class WindowHistoryRecorder(Recorder):
 
     def __init__(self):
         self.wh_dict = {}
+        self.res = self.get_screen_res()
         self.rec = False
         self.gui_ready = threading.Event()
         self.gui_thread = threading.Thread(target=self.run_gui_thread)
@@ -61,7 +86,6 @@ class WindowHistoryRecorder(Recorder):
         self.Gtk = Gtk
         self.GLib = GLib
         self.worker = threading.Thread(target=self.start_rec)
-        print('starting work...')
         self.worker.start()
     
     def start(self):
@@ -72,6 +96,6 @@ class WindowHistoryRecorder(Recorder):
 
         # TODO: put everything in the database
         for key in self.wh_dict:
-            print(key)
+            print(key.toJSON())
 
         self.dict = {}
--- backend/app/models/WindowHistory.py
+++ backend/app/models/WindowHistory.py
@@ -1,46 +1,11 @@
 from .Artifact import Artifact
 
-class Process(Artifact):
+class WindowHistory(Artifact):
     def __init__(self, timestamp, ip_address, mac_address, annotations, tags,
-    proc_name,
-    proc_user,
-    proc_pid,
-    proc_parent,
-    proc_time_created,
-    proc_cmdline,
-    proc_terminal,
-    proc_status,
-    proc_type,
-    proc_memory_percentage,
-    proc_threads,
-    proc_cpu_percentage,
-    proc_privileges,
-    proc_priority
+    screen_res
+
     ):
         super().__init__(timestamp, ip_address, mac_address, annotations, tags)
-        self.proc_name = proc_name
-        self.proc_user = proc_user
-        self.proc_pid = proc_pid
-        self.proc_parent = proc_parent
-        self.proc_time_created = proc_time_created
-        self.proc_cmdline = proc_cmdline
-        self.proc_terminal = proc_terminal
-        self.proc_status = proc_status
-        
-        if proc_type == 'running':
-            self.proc_type = 'Foreground'
-        else:
-            self.proc_type = 'Background'
-
-        self.proc_memory_percentage = proc_memory_percentage
-        self.threads = proc_threads
-        self.proc_cpu_precentage = proc_cpu_percentage
-
-        if proc_privileges == True:
-            self.proc_privileges = 'Admin'
-        else:
-            self.proc_privileges = 'Not Admin'
-        
-        self.proc_priority = proc_priority
+        self.screen_res = screen_res
     
 
