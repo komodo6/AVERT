c6f8e6cc7076b79ff473ceab477abe6c150da13e
emendoza22 <eri.men13@gmail.com>
2021-11-02 15:12:10 -0600

    video is working for screen recorder

--- backend/app/Recorder/Video/VideoRecorder.py
+++ backend/app/Recorder/Video/VideoRecorder.py
@@ -3,6 +3,11 @@ import pyautogui
 import numpy as np
 import pymongo
 import gridfs
+import uuid
+
+# X and Y coordinates of mouse pointer
+Xs = [0,8,6,14,12,4,2,0]
+Ys = [0,2,4,12,14,6,8,0]
 
 client = pymongo.MongoClient(host="54.215.46.201", port=27017)
 db = client.AVERT
@@ -11,18 +16,35 @@ testCollection = db['myImageCollection']
 fs = gridfs.GridFS(db)
 
 SCREEN_SIZE = pyautogui.size()
-FPS = 15
+FPS = 5
+
+videoname = uuid.uuid1()
+videoNameString = str(videoname)
 
 codec = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter("output.avi", codec, FPS, SCREEN_SIZE)
+#
+out = cv2.VideoWriter("./Videos/" + videoNameString + ".avi", codec, FPS, SCREEN_SIZE)
+# X and Y coordinates of mouse pointer
+Xs = [0,8,6,14,12,4,2,0]
+Ys = [0,2,4,12,14,6,8,0]
 
 while True:
     img = pyautogui.screenshot()
+    mouseX,mouseY = pyautogui.position()
+    mouseX *= 2
+    mouseY *= 2
 
     frame = np.array(img)
 
     #to turn it into the right colors, but it was complaining, will fix later
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Synthesize mouse pointer
+    Xthis = [2*x+mouseX for x in Xs]
+    Ythis = [2*y+mouseY for y in Ys]
+    points = list(zip(Xthis,Ythis))
+    points = np.array(points, 'int32')
+    cv2.fillPoly(frame,[points],color=[255,255,255])
 
     out.write(frame)
 
