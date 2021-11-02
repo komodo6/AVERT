51a15da14bd6c7e22eb6a408810f13dfd8a0a1ca
Cristobal Medina <cristobalmd01@gmail.com>
2021-11-01 21:37:35 -0600

    fixed error with screenshots

--- backend/app/Screenshots/ScreenshotRecorder.py
+++ backend/app/Screenshots/ScreenshotRecorder.py
@@ -5,7 +5,6 @@ from .CaptureScreenshot import CaptureScreenshot
 
 class ScreenshotRecorder():
     def __init__(self):
-        self.cs = CaptureScreenshot()
         self.mouse_listener = mouse.Listener(on_click=self.on_click)
         self.keyboard_listener = keyboard.Listener(on_release=self.on_release)
         self.running = False
@@ -14,15 +13,17 @@ class ScreenshotRecorder():
         if self.running:
             print('{0} released'.format(key))
             if key == Key.esc or key == Key.enter or key == Key.tab:
-                self.cs.start()
-                self.cs.join()
+                cs = CaptureScreenshot()
+                cs.start()
+                cs.join()
         else:
             return False
 
     def on_click(self, x, y, button, pressed):
         if self.running:
-            self.cs.start()
-            self.cs.join()
+            cs = CaptureScreenshot()
+            cs.start()
+            cs.join()
         else:
             return False
 
