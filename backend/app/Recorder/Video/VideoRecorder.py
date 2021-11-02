b5a5efce18cb8dcb0d6f4e2119c99ce0768a24f9
emendoza22 <eri.men13@gmail.com>
2021-11-01 23:36:51 -0600

    trying to get this to work :(

--- backend/app/Recorder/Video/VideoRecorder.py
+++ backend/app/Recorder/Video/VideoRecorder.py
@@ -1,62 +1,57 @@
 import cv2
 import pyautogui
 import numpy as np
-import time
-from threading import Thread
-from pymongo import MongoClient
-import gridfs
-from flask import Flask
 import pymongo
-
-def recorder():
-
-    SCREEN_SIZE = pyautogui.size()
-    FPS = 10
-
-    codec = cv2.VideoWriter_fourcc(*"XVID")
-    out = cv2.VideoWriter("./output.avi", codec, FPS, SCREEN_SIZE)
-
-    while True:
-        img = pyautogui.screenshot()
-
-        frame = np.array(img)
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        out.write(frame)
-
-        #cv2.imshow('Live', frame)
-
-        if cv2.waitKey(1) == ord("q"):
-            break
-
-    cv2.destroyAllWindows()
-    out.release()
+import gridfs
 
 client = pymongo.MongoClient(host="54.215.46.201", port=27017)
-
 db = client.AVERT
-
-testCollection = db['myVideoCollection']
+testCollection = db['myImageCollection']
 
 fs = gridfs.GridFS(db)
 
-videoString = frame.tostring()
-videoID = fs.put(videoString, encoding='utf-8')
+SCREEN_SIZE = pyautogui.size()
+FPS = 15
 
+codec = cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("output.avi", codec, FPS, SCREEN_SIZE)
+
+while True:
+    img = pyautogui.screenshot()
+
+    frame = np.array(img)
+
+    #to turn it into the right colors, but it was complaining, will fix later
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    out.write(frame)
+
+    #cv2.imshow('Live', frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+out.release
+cv2.destroyAllWindows()
+
+#turning into a string, but not really sure if that works for videos
+videoString = out.tostring()
+
+# store the image
+imageID = fs.put(videoString, encoding='utf-8')
+
+
+# create our image meta data
 meta = {
-    'name': 'myTestSet'
-    # 'videos': [
-    #     {
-    #         'imageID': videoID,
-    #         'shape': frame.shape,
-    #         'dtype': str(frame.dtype)
-    #     }
-    # ]
+    'name': 'myTestSet',
+    'video': [
+        {
+            'videoID': videoID,
+            'shape': out.shape,
+            'dtype': str(out.dtype)
+        }
+    ]
 }
 
-print(meta)
+# insert the meta data
 testCollection.insert_one(meta)
-
-if __name__ "__main__":
-    
