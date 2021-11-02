0ee9038a428258fd849b1f4c47d755c4c931525d
emendoza22 <eri.men13@gmail.com>
2021-11-01 20:53:09 -0600

    updating mongodb issue

--- backend/app/Recorder/Video/VideoRecorder.py
+++ backend/app/Recorder/Video/VideoRecorder.py
@@ -5,48 +5,58 @@ import time
 from threading import Thread
 from pymongo import MongoClient
 import gridfs
+from flask import Flask
+import pymongo
 
-client = MongoClient('localhost', 27017)
-db = client['testDatabaseONE']
+def recorder():
+
+    SCREEN_SIZE = pyautogui.size()
+    FPS = 10
+
+    codec = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter("./output.avi", codec, FPS, SCREEN_SIZE)
+
+    while True:
+        img = pyautogui.screenshot()
+
+        frame = np.array(img)
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        out.write(frame)
+
+        #cv2.imshow('Live', frame)
+
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    out.release()
+
+client = pymongo.MongoClient(host="54.215.46.201", port=27017)
+
+db = client.AVERT
+
 testCollection = db['myVideoCollection']
 
 fs = gridfs.GridFS(db)
-
-SCREEN_SIZE = pyautogui.size()
-FPS = 10
-
-codec = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter("./output.avi", codec, FPS, SCREEN_SIZE)
-
-while True:
-    img = pyautogui.screenshot()
-
-    frame = np.array(img)
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    out.write(frame)
-
-    #cv2.imshow('Live', frame)
-
-    if cv2.waitKey(1) == ord("q"):
-        break
-
-cv2.destroyAllWindows()
-out.release()
 
 videoString = frame.tostring()
 videoID = fs.put(videoString, encoding='utf-8')
 
 meta = {
-    'name': 'myTestSet',
-    'videos': [
-        {
-            'imageID': videoID,
-            'shape': frame.shape,
-            'dtype': str(frame.dtype)
-        }
-    ]
+    'name': 'myTestSet'
+    # 'videos': [
+    #     {
+    #         'imageID': videoID,
+    #         'shape': frame.shape,
+    #         'dtype': str(frame.dtype)
+    #     }
+    # ]
 }
 
+print(meta)
 testCollection.insert_one(meta)
+
+if __name__ "__main__":
+    
