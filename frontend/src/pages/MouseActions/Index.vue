da39e3b5c061246d88893215db72ebf12f4a1564
Cristobal Medina <cristobalmd01@gmail.com>
2021-11-03 02:12:22 -0600

    fixed component

--- frontend/src/pages/MouseActions/Index.vue
+++ frontend/src/pages/MouseActions/Index.vue
@@ -21,7 +21,7 @@
                 <q-icon name="search"> </q-icon>
               </template>
             </q-input>
-            <ExportData :props="{rows: rows, columns: columns}"/>
+            <ExportData :rows="rows" :columns="columns"/>
           </template>
           <template v-slot:body="props">
             <q-tr :props="props">
