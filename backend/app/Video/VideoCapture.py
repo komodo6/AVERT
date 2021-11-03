3857f989f6f9f92667ae719401fdaba5823d0400
emendoza22 <eri.men13@gmail.com>
2021-11-03 01:51:43 -0600

    updated video resolution

--- backend/app/Video/VideoCapture.py
+++ backend/app/Video/VideoCapture.py
@@ -3,19 +3,21 @@ from .VideoDAO import VideoDAO
 from threading import Thread
 import os
 import uuid
+from vidgear.gears import ScreenGear
+from vidgear.gears import WriteGear
+import cv2
 from ..Recorder.Recorder import Recorder
 
 r = Recorder()
 
 
 # define dimensions of screen w.r.t to given monitor to be captured
-options = {"top": 40, "left": 100, "width": 100, "height": 100}
+options = {}
 
 # define suitable (Codec,CRF,preset) FFmpeg parameters for writer
 output_params = {"-vcodec": "libx264", "-crf": 0, "-preset": "fast"}
-
 # open video stream with defined parameters
-stream = ScreenGear(monitor=1, logging=True, **options).start()
+stream = ScreenGear(monitor=1, logging=False, **options).start()
 
 # Define writer with defined parameters and suitable output filename for e.g. Output.mp4
 
@@ -32,7 +34,7 @@ class VideoCapture(Thread):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         
         id = str(uuid.uuid4())
-        writer = WriteGear(output_filename=f"./videos/{id}.mp4", logging=True, **output_params)
+        writer = WriteGear(output_filename=f"./videos/{id}.mp4", logging=False, **output_params)
         # loop over
         while True:
 
@@ -45,13 +47,14 @@ class VideoCapture(Thread):
 
             # {do something with the frame here}
             # lets convert frame to gray for this example
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             # write gray frame to writer
-            writer.write(gray)
+            writer.write(frame)
 
             # Show output window
-            cv2.imshow("Output Gray Frame", gray)
+            cv2.imshow("Output Gray Frame", frame)
 
             # check for 'q' key if pressed
             key = cv2.waitKey(1) & 0xFF
@@ -69,7 +72,7 @@ class VideoCapture(Thread):
 
 
 
-        # TODO: fix attributes
+     # TODO: fix attributes
         s = Video(r.get_timestamp(), r.get_ip(),
                         r.get_mac(), [], [], resolution="idk", frameRate="id", filename=f"{id}.mp4")
         self.vdao.create(s)
