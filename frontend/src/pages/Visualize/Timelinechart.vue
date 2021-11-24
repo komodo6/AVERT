60c45236e32ad9ac816018b99e489885bd47b3cb
landinij0040 <ijandin@miners.utep.edu>
2021-11-24 12:21:57 -0700

    Chnage Timeline from setup to created lifecycle

--- frontend/src/pages/Visualize/Timelinechart.vue
+++ frontend/src/pages/Visualize/Timelinechart.vue
@@ -24,6 +24,7 @@ import {
   fetchSystemCalls,
   fetchVideos,
 } from "src/utils/request.js";
+import axios from 'axios'; 
 // import {chartOptions} from "./timelineoption.js";
 
 export default {
@@ -31,14 +32,27 @@ export default {
   components: {
     apexchart: VueApexCharts,
   },
-  setup() {
-    let series = ref([]);
-    let chartOptions = ref({});
-    fetchMouseActionsTimeline();
-    onMounted(() => {
-      console.log(avertStore.state.mouseActionsTimeline);
+  async created(){
 
-      chartOptions.value = {
+    const maTimeline = await axios.post("http://localhost:5000/mouseactions/timeline", {
+      start: 'start',
+      end: 'end',
+    })
+    console.log('maTimeline' + JSON.stringify(maTimeline) )
+    console.log(maTimeline['data']['r_intervals'])
+
+
+
+
+    this.series= [
+        {
+          name: "Mouse Action",
+          data:maTimeline['data']['r_intervals'],
+        }
+      ];
+
+
+    this.chartOptions = {
         chart: {
           height: 350,
           type: "line",
@@ -53,26 +67,123 @@ export default {
           width: [5, 5, 4],
           curve: "straight",
         },
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
         title: {
           text: "Missing data (null values)",
         },
         xaxis: {
-            categories:avertStore.state.mouseActionsTimeline['r_times']
+            categories:maTimeline['data']['r_times']
         },
-      };
-
-      series.value = [
+      }; 
+  },
+  data(){
+    return{
+        series: [
         {
           name: "Mouse Action",
-          data:avertStore.state.mouseActionsTimeline['r_intervals'],
+          data:[],
         }
-      ];
-    });
-    return {
-      chartOptions,
-      series,
-    };
-  },
+      ],
+      chartOptions : {
+        chart: {
+          height: 350,
+          type: "line",
+          zoom: {
+            enabled: false,
+          },
+          animations: {
+            enabled: true,
+          },
+        },
+        stroke: {
+          width: [5, 5, 4],
+          curve: "straight",
+        },
+        title: {
+          text: "Missing data (null values)",
+        },
+        xaxis: {
+            categories:[]
+        },
+      } 
+    }
+  }
+  // setup() {
+  //   let series = ref([]);
+  //   series.value = [
+  //       {
+  //         name: "Mouse Action",
+  //         data:[],
+  //       }
+  //     ];
+
+
+
+  //   let chartOptions = ref({});
+
+  //   chartOptions.value = {
+  //       chart: {
+  //         height: 350,
+  //         type: "line",
+  //         zoom: {
+  //           enabled: false,
+  //         },
+  //         animations: {
+  //           enabled: true,
+  //         },
+  //       },
+  //       stroke: {
+  //         width: [5, 5, 4],
+  //         curve: "straight",
+  //       },
+  //       labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+  //       title: {
+  //         text: "Missing data (null values)",
+  //       },
+  //       xaxis: {
+  //           categories:[]
+  //       },
+  //     };
+
+
+
+    
+  //   onMounted(() => {
+  //     console.log(avertStore.state.mouseActionsTimeline);
+  //     fetchMouseActionsTimeline();
+  //     chartOptions.value = {
+  //       chart: {
+  //         height: 350,
+  //         type: "line",
+  //         zoom: {
+  //           enabled: false,
+  //         },
+  //         animations: {
+  //           enabled: true,
+  //         },
+  //       },
+  //       stroke: {
+  //         width: [5, 5, 4],
+  //         curve: "straight",
+  //       },
+  //       title: {
+  //         text: "Missing data (null values)",
+  //       },
+  //       xaxis: {
+  //           categories:avertStore.state.mouseActionsTimeline['r_times']
+  //       },
+  //     };
+
+  //     series.value = [
+  //       {
+  //         name: "Mouse Action",
+  //         data:avertStore.state.mouseActionsTimeline['r_intervals'],
+  //       }
+  //     ];
+  //   });
+  //   return {
+  //     chartOptions,
+  //     series,
+  //   };
+  // },
 };
 </script>
